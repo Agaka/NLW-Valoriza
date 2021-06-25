bbe1e735d032bbdbe85ccc7d18942b390bb9ab9d
Allan Gabriel Sobrero dos Santos <55900557+lAgakal@users.noreply.github.com>
2021-06-25 17:33:22 -0300

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -3,17 +3,20 @@ import { CreateUserController } from "./controllers/CreateUserController"
 import { CreateTagController } from "./controllers/CreateTagController"
 import { ensureAdmin } from "./middlewares/ensureAdmin"
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController"
+import { CreateComplimentController } from "./controllers/CreateComplimentController"
 
 const router = Router()
 
 const createUserController = new CreateUserController()
 const createTagController = new CreateTagController()
 const authenticateUserControler = new AuthenticateUserController()
+const complimentController = new CreateComplimentController()
 
 
 router.post("/users", createUserController.handle)
 router.post("/tags", ensureAdmin, createTagController.handle)
 router.post("/login", authenticateUserControler.handle)
+router.post("/compliments", complimentController.handle)
 
 
 export { router }
