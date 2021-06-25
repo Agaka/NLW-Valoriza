8d8d704c09a8689f9418a4f9086e265c6538bd37
Allan Gabriel Sobrero dos Santos <55900557+lAgakal@users.noreply.github.com>
2021-06-25 17:06:12 -0300

    main

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,45 @@
+import { getCustomRepository } from "typeorm"
+
+import { compare } from "bcryptjs"
+import { sign } from "jsonwebtoken"
+
+import { UsersRepositories } from "../repositories/UsersRepositories"
+
+interface IAuthenticateRequest {
+    email: string 
+    password: string
+
+}
+
+class AuthenticateUserService {
+
+    async execute({ email, password }: IAuthenticateRequest) {
+        const usersRepository = getCustomRepository(UsersRepositories)
+
+        const user = await usersRepository.findOne({
+            email
+        })
+
+        if(!user) {
+            throw new Error("Email/Password incorrect")
+        }
+
+        const passwordMatch = await compare(password, user.password)
+
+        if(!passwordMatch) {
+            throw new Error("Email/Password incorrect")
+        }
+
+        const token = sign({
+            email: user.email,
+        }, "ae3273d9be2cdadc3d8b4cff77f69717", {
+            subject: user.id,
+            expiresIn: "1d"
+        })
+
+        return token 
+    }
+
+}
+
+export {AuthenticateUserService}
